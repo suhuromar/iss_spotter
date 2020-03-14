21c9f4b9aa1b42de328715110599c2388acf16d6
Suhur Omar Farah <itssuhuromar@gmail.com>
2020-03-14 21:42:48 +0000

    promises

---
+++ index2.js
@@ -0,0 +1,22 @@
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyoverTimes } = require('./iss_promised');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyoverTimes)
+//   .then(body => console.log(body))
+
+
+// index2.js
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't: " , error.message);
+  });
